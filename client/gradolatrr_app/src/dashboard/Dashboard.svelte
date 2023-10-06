e4e41e911171295c4e8a569cd6a32b5faae73e13
Ada <adabingw@yahoo.com>
2023-10-05 20:02:06 -0400

    :sake:

--- client/gradolatrr_app/src/dashboard/Dashboard.svelte
+++ client/gradolatrr_app/src/dashboard/Dashboard.svelte
@@ -8,9 +8,9 @@
     <p>welcome to gradolatrr. <a href="https://github.com/adabingw/gradolatrr" target="_blank"><i class="fa-brands fa-github"></i></a></p>
     <div class="journey">
         this is version 3 of this project. <br />
-        version 1: August 2021. written with Java AWT and a MySQL backend. <br />
-        version 2: August 2022. written as a desktop app (using Electron) with a PERN stack. <br/>
-        version 3: August 2023. written with Svelte and a DynamoDB backend, integrated with GraphQL and AWS AppSync.
+        version 1: August 2021. Java AWT and MySQL. <br />
+        version 2: August 2022. (using Electron) with a PERN stack. <br/>
+        version 3: August 2023. Svelte and DynamoDB, integrated with GraphQL and AWS AppSync.
     </div>
 </div>
 
@@ -23,10 +23,15 @@ i:hover {
     cursor: pointer;
 }
 
+p {
+    font-weight: bold;
+    margin-bottom: 15px;
+}
+
 .dashboard {
     width: 100%;
     height: 100%;
-    text-align: center;
+    text-align: left;
     margin-left: -50px;
     display: flex;
     justify-content: center;
